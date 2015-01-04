d83bd4d0c4f875d7d3215b391cfc6c0d52b5eac4
Julien Sadaoui <julien@MBP-de-Julien.home>
2015-01-04 16:32:46 +0100

    Simple web application with integration tests

--- src/main/java/org/jsadaoui/demo/web/HomeController.java
+++ src/main/java/org/jsadaoui/demo/web/HomeController.java
@@ -1,13 +1,16 @@
 package org.jsadaoui.demo.web;
 
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HomeController {
 
-    @RequestMapping("/")
+    @RequestMapping(value = "/")
     public String home() {
-        return "Hello ParisJUG!";
+        return "Hello, world.";
     }
 }
---
+++ src/test/java/org/jsadaoui/demo/web/HomeControllerTest.java
@@ -0,0 +1,49 @@
+package org.jsadaoui.demo.web;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+
+import org.jsadaoui.demo.Application;
+import org.jsadaoui.demo.web.HomeController;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@SpringApplicationConfiguration(classes = Application.class)
+@IntegrationTest("server.port:9000")
+public class HomeControllerTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mvc;
+
+    @Before
+    public void setUp() {
+        this.mvc = webAppContextSetup(this.wac).build();
+    }
+
+    @Test
+    public void testHome() throws Exception {
+        mvc.perform(get("/").accept(MediaType.TEXT_PLAIN))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_PLAIN + ";charset=ISO-8859-1"))
+                .andExpect(content().string("Hello, world."));
+    }
+}
